ac1c210533f1df3a91dc0f6cfd6e1529f83ba63a
younesbelkada <younesbelkada@gmail.com>
2022-10-07 13:43:23 +0200

    v1 inference

--- distill_bloom/__init__.py
+++ distill_bloom/__init__.py
@@ -3,3 +3,4 @@ from .arguments.arguments import parse_args
 from .dataset.get_dataset import build_train_val_test_dataset
 
 # Arguments import
+from .init_wrapper import DeepSpeedInitWrapper, print_rank0
